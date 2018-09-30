30447109605a3933c7f1204517e0660df7a91992
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-09-30 14:15:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 function takeANumber(currentLine, newPersonName){
+  currentLine.push(newPersonName);
   
+  return "Welcome, " + newPersonName + ". You are number " + currentLine.length + " in line."
 }
